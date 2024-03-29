daebccb0916c8a8d387cbb03084ffbca487cd3bf
Muhammad Sohaib <bhattisohaib11@gmail.com>
2024-03-29 15:54:35 -0400

    main page animation

--- src/UI/JobSearchbar.jsx
+++ src/UI/JobSearchbar.jsx
@@ -1,12 +1,24 @@
 import { FaSearch } from "react-icons/fa";
+import { useEffect, useState } from "react";
 
 const JobSearchbar = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  // Set isVisible to true when the component mounts
+  useEffect(() => {
+    setIsVisible(true);
+  }, []);
+
   return (
-    <div className="flex-col    rounded-sm md:flex sm:flex-row pt-3 px-2 pb-2 md:gap-4 bg-blurr-bg">
-      <div className="text-sm  w-full">
+    <div
+      className={`flex-col rounded-sm md:flex sm:flex-row pt-3 px-2 pb-2 md:gap-4 bg-blurr-bg ${
+        isVisible ? "transition-transform transform translate-y-0 duration-500 ease-out" : "transition-transform transform translate-y-full duration-500 ease-in"
+      }`}
+    >
+      <div className="text-sm w-full">
         <p className="text-white"> Job Title </p>
-        <div className=" flex  items-center   rounded-md    bg-white px-2">
-          <FaSearch className="text-btn-primary " />
+        <div className="flex items-center rounded-md bg-white px-2">
+          <FaSearch className="text-btn-primary" />
           <input
             type="text rounded-md"
             className="p-5 pe-16 outline-none w-full rounded-md"
@@ -14,22 +26,22 @@ const JobSearchbar = () => {
           />
         </div>
       </div>
-      <div className="text-sm  w-full">
+      <div className="text-sm w-full">
         <p className="text-white">Where</p>
         <div className="">
           <input
             type="text   "
-            className="p-5 outline-none rounded-md border    px-10 w-full"
+            className="p-5 outline-none rounded-md border px-10 w-full"
             placeholder="Location"
           />
         </div>
       </div>
-      <div className=" flex flex-col sm:gap-0   gap-4">
-        <div className="flex gap-1  items-center mt-2 sm:mt-0 ">
+      <div className="flex flex-col sm:gap-0 gap-4">
+        <div className="flex gap-1 items-center mt-2 sm:mt-0 ">
           <input type="checkbox" className="text-black" name="" id="" />
           <p className="text-sm text-white">Remote Only</p>
         </div>
-        <button className="px-10 text-sm rounded-md  font-bold text-white py-[22px] bg-[#4e007a]">
+        <button className="px-10 text-sm rounded-md font-bold text-white py-6 bg-[#4e007a]">
           Search
         </button>
       </div>
--- src/UI/SearchJobsMain.jsx
+++ src/UI/SearchJobsMain.jsx
@@ -1,14 +1,29 @@
-// import bgimg from "../assets";
 import JobSearchbar from "./JobSearchbar";
+import { useEffect, useState } from "react";
+
 const SearchJobsMain = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  // Set isVisible to true when the component mounts
+  useEffect(() => {
+    setIsVisible(true);
+  }, []);
+
   return (
-    <div className=" py-12 px-8  main-banner  flex flex-col gap-2    items-center">
-      <p className="text-2xl font-medium   text-center">
+    <div className="py-12 px-8 main-banner flex flex-col gap-2 items-center">
+      <p
+        className={`text-2xl font-medium text-center ${
+          isVisible ? "transition-transform transform translate-y-0 opacity-500" : "transition-transform transform -translate-y-full opacity-500"
+        }`}
+      >
         Join us & Explore Thousands of Jobs
       </p>
-      <p className="text-sm  text-center hidden sm:block  ">
-        Select your industry and/or type your keyword, then click search to find
-        your perfect job
+      <p
+        className={`text-sm text-center ${
+          isVisible ? "transition-transform transform translate-y-0 opacity-500" : "transition-transform transform -translate-y-full opacity-500"
+        }`}
+      >
+        Select your industry and/or type your keyword, then click search to find your perfect job
       </p>
       <JobSearchbar />
     </div>
