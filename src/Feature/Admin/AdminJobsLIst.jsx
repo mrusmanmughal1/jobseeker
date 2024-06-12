59a151bb969ecb5078c550dc50376092fd7489ff
sohaib bhatti <bhattisohaib11@gmail.com>
2024-06-13 01:39:33 +0500

    Job list and Emplyer list

--- src/Feature/Admin/AdminJobsLIst.jsx
+++ src/Feature/Admin/AdminJobsLIst.jsx
@@ -8,11 +8,11 @@ const AdminJobsList = () => {
   const aa = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   return (
     <div className="flex flex-col gap-4">
-      <Adminfilters/>
+      <Adminfilters />
       {aa.map((v, i) => (
         <div
           key={i}
-          className="flex flex-col md:flex-row gap-4 p-5 shadow-lg border-2 border-b 
+          className="flex flex-col md:flex-row gap-4 p-5 shadow-lg border-2 border-b
           hover:bg-slate-100 bg-white"
         >
           <div className="w-full md:w-1/3 flex flex-col gap-4">
@@ -22,8 +22,12 @@ const AdminJobsList = () => {
             </p>
           </div>
           <div className="w-full md:w-1/3 text-sm p-2 font-bold">
-            <p>Salary :  <span  className="text-sm font-medium">20$ /hr</span> </p>
-            <p>Job Type : <span className="font-medium">Contract</span></p>
+            <p>
+              Salary : <span className="text-sm font-medium">20$ /hr</span>{" "}
+            </p>
+            <p>
+              Job Type : <span className="font-medium">Contract</span>
+            </p>
           </div>
           <div className="md:w-1/3 w-full flex justify-end gap-4 md:flex-row md:items-center text-purple-900">
             <div className="flex gap-4">
